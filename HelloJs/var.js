f49de412e1e2583dd9167de7a7a1ca484c14a71d
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-09-23 22:46:04 +0600

    number type in js

--- HelloJs/var.js
+++ HelloJs/var.js
@@ -4,4 +4,6 @@ var weight = 58;
 var price = 350;
 var year = 2021;
 
+var age = 24;
 console.log(price, year, weight);
+console.log(age);
---
+++ num.js
@@ -0,0 +1,6 @@
+var applePrice = 250;
+var chocolatePrice = 300;
+var bananaPrice = '10';
+
+console.log(applePrice + chocolatePrice)
+// console.log(typeof (bananaPrice));
